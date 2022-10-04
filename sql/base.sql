a492752eaca80690be0b481d7c6fa3dd553256ef
Apeul <martinsla@live.fr>
2022-10-04 13:10:16 +0200

    bath no more culture

--- sql/base.sql
+++ sql/base.sql
@@ -590,9 +590,9 @@ INSERT OR IGNORE INTO Requirements (RequirementId, RequirementType) VALUES
 	('HAS_CODE_OF_LAWS_BBG', 'REQUIREMENT_PLAYER_HAS_CIVIC');
 INSERT OR IGNORE INTO RequirementArguments (RequirementId, Name, Value) VALUES
 	('HAS_CODE_OF_LAWS_BBG', 'CivicType', 'CIVIC_CODE_OF_LAWS');
--- Baths get Culture minor adjacency bonus added
-INSERT OR IGNORE INTO District_Adjacencies (DistrictType , YieldChangeId)
-	VALUES ('DISTRICT_BATH' , 'District_Culture');
+-- reverted 04/10/22
+-- INSERT OR IGNORE INTO District_Adjacencies (DistrictType , YieldChangeId)
+-- 	VALUES ('DISTRICT_BATH' , 'District_Culture');
 
 
 --==================
