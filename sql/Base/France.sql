1ee713cb0f9d9e8fab7aaf3a5844eadce4ea8148
FlashyFeeds <popanuilia@gmail.com>
2022-10-25 20:54:41 +0200

    France Spy Promotions
    Fixed bug where France Spy gets extra promotions upon changing cities
    2 Step attachment mechanism instead of 1. France modifier was buggy because of how spy works on core side. When it moves from city to city it gets deleted in the first city and recreated in the second city, which triggered France modifier to trigger over and over
    Bug description:
    https://discord.com/channels/1008169124070957076/1023137410919436289

--- sql/Base/France.sql
+++ sql/Base/France.sql
@@ -2,6 +2,15 @@
 UPDATE TraitModifiers SET TraitType='TRAIT_CIVILIZATION_WONDER_TOURISM' WHERE TraitType='FLYING_SQUADRON_TRAIT' AND ModifierId='UNIQUE_LEADER_ADD_SPY_CAPACITY';
 UPDATE TraitModifiers SET TraitType='TRAIT_CIVILIZATION_WONDER_TOURISM' WHERE TraitType='FLYING_SQUADRON_TRAIT' AND ModifierId='UNIQUE_LEADER_ADD_SPY_UNIT';
 UPDATE TraitModifiers SET TraitType='TRAIT_CIVILIZATION_WONDER_TOURISM' WHERE TraitType='FLYING_SQUADRON_TRAIT' AND ModifierId='UNIQUE_LEADER_SPIES_START_PROMOTED';
+
+--Bugfix: French spy extra promotion choice on changing cities.
+UPDATE Modifiers SET ModifierType = 'MODIFIER_PLAYER_CITIES_ATTACH_MODIFIER', SubjectRequirementSetId = NULL WHERE ModifierId = 'UNIQUE_LEADER_SPIES_START_PROMOTED';
+INSERT INTO Modifiers(ModifierId, ModifierType, SubjectRequirementSetId) VALUES
+	('BBG_LEADER_SPIES_START_PROMOTED_CITY_ATTACH', 'MODIFIER_CITY_TRAINED_UNITS_ADJUST_GRANT_EXPERIENCE', 'UNIT_IS_SPY');
+INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
+	('BBG_LEADER_SPIES_START_PROMOTED_CITY_ATTACH', 'Amount', '-1'),
+	('UNIQUE_LEADER_SPIES_START_PROMOTED','ModifierId','BBG_LEADER_SPIES_START_PROMOTED_CITY_ATTACH');
+
 -- Reduce tourism bonus for wonders
 UPDATE ModifierArguments SET Value='150' WHERE ModifierId='TRAIT_WONDER_DOUBLETOURISM' AND Name='ScalingFactor';
 -- Chateau now gives 1 housing at Feudalism, and ajacent luxes now give stacking Culture in addition to stacking gold
