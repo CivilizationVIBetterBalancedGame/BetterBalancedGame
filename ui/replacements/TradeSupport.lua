0322cb415ae3522fd5010100dbb482bafc8b4e53
FlashyFeeds <popanuilia@gmail.com>
2022-10-25 11:32:35 +0200

    Display bugs
    -Fixed Hungary Huszair display bug
    Overrides UnitPanel_XP2, english loc included
    
    Mechanics:
    Added the baseline modifier that adds +0 to huszair. Attached a modifier string so it gets displayed by ui.
    Override UnitPanel UI to show total amount of CS and concise list of citystates. Having +2 in each line and 50 lines of localized text would be very annoying for all.
    Extra:
    BBG already had Unit Panel override (by JackTheNarrator) that was not active. It had condemn heretic mechanic. I renamed his override to unitpanel_bbg_legacy, so it still hangs around like it used to and new override is called UnitPanel_BBG.lua and is activated in the modinfo
    
    Potential Problem: BSM overrides the same panel, need to add action criteria in BSM that goes like if BBG5 active overide the BBG5 panel instead of XP2 panel like it is now. So expect new BSM for this conflict resolution and fix of the Writing boost with spectator.
    
    Fixed Portugal Trade Routs Preview
    Replaces the support script TradeSupport.lua. It is actually a Firaxis indexing bug C++ indexing starts with 0, while lua starts with 1(simillar to no available trade routs from capital bug fixed by Vodario). TradeSupport is updated with the same name, so it works clean with all the other mods (because firaxis engine loads the last queued file with the same name). No other of our mods replaces this file, so it is 100% safe.
    
    Modinfo changed to 5.1.2
    and respective necessary lines added for both fixes.
    
    P.S. Didn't want to separate these 2 PR's to not make mistakes in modinfo and risk having the error: One or More Mods Failed to load.

---
+++ ui/replacements/TradeSupport.lua
@@ -0,0 +1,189 @@
+print("TradeSupport BBG overide loaded")
+
+-- Get idle Trade Units by Player ID
+function GetIdleTradeUnits( playerID:number )
+	local idleTradeUnits:table = {};
+
+	-- Loop through the Players units
+	local localPlayerUnits:table = Players[playerID]:GetUnits();
+	for i,unit in localPlayerUnits:Members() do
+
+		-- Find any trade units
+		local unitInfo:table = GameInfo.Units[unit:GetUnitType()];
+		if unitInfo.MakeTradeRoute then
+			local doestradeUnitHasRoute:boolean = false;
+
+			-- Determine if those trade units are busy by checking outgoing routes from the players cities
+			local localPlayerCities:table = Players[playerID]:GetCities();
+			for i,city in localPlayerCities:Members() do
+				local routes = city:GetTrade():GetOutgoingRoutes();
+				for i,route in ipairs(routes) do
+					if route.TraderUnitID == unit:GetID() then
+						doestradeUnitHasRoute = true;
+					end
+				end
+			end
+
+			-- If this trade unit isn't attached to an outgoing route then they are idle
+			if not doestradeUnitHasRoute then
+				table.insert(idleTradeUnits, unit);
+			end
+		end
+	end
+
+	return idleTradeUnits;
+end
+
+-- ===========================================================================
+function GetYieldsForRoute(pOriginCity:table, pDestinationCity:table, bReturnDestiationYields:boolean)
+	local kRouteInfo:table = {
+		kYieldValues = {},
+		TooltipText = "",
+		HasPathBonus = false,
+		MajorityReligion = -1,
+		ReligionPressure = -1
+	}
+
+	if pOriginCity == nil or pDestinationCity == nil then
+		return kRouteInfo;
+	end
+
+	local pTradeManager = Game.GetTradeManager();
+
+	-- Default to origin yields
+	if bReturnDestiationYields == nil then
+		bReturnDestiationYields = false;
+	end
+
+	local originOwnerID:number = pOriginCity:GetOwner();
+	local originCityID:number = pOriginCity:GetID();
+
+	local destOwnerID:number = pDestinationCity:GetOwner();
+	local destCityID:number = pDestinationCity:GetID();
+
+	-- From route
+	local kRouteYields:table = {};
+	if not bReturnDestiationYields then
+		kRouteYields = pTradeManager:CalculateOriginYieldsFromPotentialRoute(originOwnerID, originCityID, destOwnerID, destCityID);
+	else
+		kRouteYields = pTradeManager:CalculateDestinationYieldsFromPotentialRoute(originOwnerID, originCityID, destOwnerID, destCityID);
+	end
+
+	-- From path
+	local kPathYields:table = {};
+	if not bReturnDestiationYields then
+		kPathYields = pTradeManager:CalculateOriginYieldsFromPath(originOwnerID, originCityID, destOwnerID, destCityID);
+	else
+		kPathYields = pTradeManager:CalculateDestinationYieldsFromPath(originOwnerID, originCityID, destOwnerID, destCityID);
+	end
+
+	-- From modifiers
+	local kModifierYields:table = {};
+	if not bReturnDestiationYields then
+		kModifierYields = pTradeManager:CalculateOriginYieldsFromModifiers(originOwnerID, originCityID, destOwnerID, destCityID);
+	else
+		kModifierYields = pTradeManager:CalculateDestinationYieldsFromModifiers(originOwnerID, originCityID, destOwnerID, destCityID);
+	end
+
+	-- Overall modifiers / multipliers
+	local kYieldMultipliers:table = {};
+	for yieldIndex=1, #kRouteYields, 1 do
+		kYieldMultipliers[yieldIndex] = 1;
+		if originOwnerID ~= destOwnerID then
+			if not bReturnDestiationYields then
+				local pPlayerTrade:table = Players[originOwnerID]:GetTrade();
+				kYieldMultipliers[yieldIndex] = pPlayerTrade:GetInternationalYieldModifier(yieldIndex-1);
+			else
+				local pPlayerTrade:table = Players[destOwnerID]:GetTrade();
+				kYieldMultipliers[yieldIndex] = pPlayerTrade:GetInternationalYieldModifier(yieldIndex-1);
+			end
+		end
+	end
+
+	-- Add the yields together and return the result
+	for yieldIndex=1, #kRouteYields, 1 do
+
+		local kYieldInfo:table = GameInfo.Yields[yieldIndex - 1];
+		if kYieldInfo ~= nil then
+			local routeValue:number = kRouteYields[yieldIndex];
+			if routeValue > 0 then
+				if kRouteInfo.TooltipText ~= "" then
+					kRouteInfo.TooltipText = kRouteInfo.TooltipText .. "[NEWLINE]";
+				end
+				kRouteInfo.TooltipText = kRouteInfo.TooltipText .. Locale.Lookup("LOC_ROUTECHOOSER_YIELD_SOURCE_DISTRICTS", routeValue, kYieldInfo.IconString, kYieldInfo.Name, pDestinationCity:GetName());
+			end
+
+			local pathValue:number = kPathYields[yieldIndex];
+			if pathValue > 0 then
+				kRouteInfo.HasPathBonus = true;
+				if kRouteInfo.TooltipText ~= "" then
+					kRouteInfo.TooltipText = kRouteInfo.TooltipText .. "[NEWLINE]";
+				end
+				kRouteInfo.TooltipText = kRouteInfo.TooltipText .. Locale.Lookup("LOC_ROUTECHOOSER_YIELD_SOURCE_TRADING_POSTS", pathValue, kYieldInfo.IconString, kYieldInfo.Name);
+			end
+
+			local modifierValue:number = kModifierYields[yieldIndex];
+			if modifierValue > 0 then
+				if kRouteInfo.TooltipText ~= "" then
+					kRouteInfo.TooltipText = kRouteInfo.TooltipText .. "[NEWLINE]";
+				end
+				kRouteInfo.TooltipText = kRouteInfo.TooltipText .. Locale.Lookup("LOC_ROUTECHOOSER_YIELD_SOURCE_BONUSES", modifierValue, kYieldInfo.IconString, kYieldInfo.Name);
+			end
+
+			local totalBeforeMultiplier:number = routeValue + pathValue + modifierValue;
+			local total:number = totalBeforeMultiplier;
+			local multiplier:number = kYieldMultipliers[yieldIndex];
+			if total > 0 and multiplier ~= 1 then
+				total = totalBeforeMultiplier * multiplier;
+				local valueFromMultiplier:number = total - totalBeforeMultiplier;
+				local multiplierAsPercent:number = (multiplier * 100) - 100;
+
+				if kRouteInfo.TooltipText ~= "" then
+					kRouteInfo.TooltipText = kRouteInfo.TooltipText .. "[NEWLINE]";
+				end
+				kRouteInfo.TooltipText = kRouteInfo.TooltipText .. Locale.Lookup("LOC_ROUTECHOOSER_YIELD_SOURCE_MULTIPLIERS", valueFromMultiplier, kYieldInfo.IconString, kYieldInfo.Name, multiplierAsPercent);
+			end
+
+			-- Put the total into routeYields
+			kRouteInfo.kYieldValues[yieldIndex] = total;
+		end
+	end
+
+	kRouteInfo.MajorityReligion = pOriginCity:GetReligion():GetMajorityReligion();
+	if (kRouteInfo.MajorityReligion > 0) then
+		local pressureValue, sourceText = GetReligiousPressureForCity(kRouteInfo.MajorityReligion, pOriginCity, pDestinationCity, not bReturnDestiationYields);
+		if (pressureValue ~= 0) then
+			if (kRouteInfo.TooltipText ~= "") then
+				kRouteInfo.TooltipText = kRouteInfo.TooltipText .. "[NEWLINE]";
+			end
+			kRouteInfo.TooltipText = kRouteInfo.TooltipText .. sourceText;
+			kRouteInfo.ReligionPressure = pressureValue;
+		end
+	end
+
+	return kRouteInfo;
+end
+
+-- ===========================================================================
+function GetReligiousPressureForCity(religionIndex:number, originCity:table, destinationCity:table, forOriginCity:boolean)
+	local pressureValue = 0;
+	local pressureIconString = "";
+	local cityName = "";
+	local tradeManager = Game.GetTradeManager();
+
+	if originCity == nil or destinationCity == nil then
+		return 0, "";
+	end
+
+	if (forOriginCity) then
+		pressureValue = tradeManager:CalculateOriginReligiousPressureFromPotentialRoute(originCity:GetOwner(), originCity:GetID(), destinationCity:GetOwner(), destinationCity:GetID(), religionIndex);
+		pressureIconString = "[ICON_PressureLeft]";
+		cityName = destinationCity:GetName();
+	else
+		pressureValue = tradeManager:CalculateDestinationReligiousPressureFromPotentialRoute(originCity:GetOwner(), originCity:GetID(), destinationCity:GetOwner(), destinationCity:GetID(), religionIndex);
+		pressureIconString = "[ICON_PressureRight]";
+		cityName = originCity:GetName();
+	end
+	local sourceText = Locale.Lookup("LOC_ROUTECHOOSER_RELIGIOUS_PRESSURE_SOURCE_MAJORITY_RELIGION", pressureValue, pressureIconString, Game.GetReligion():GetName(religionIndex), cityName);
+	return pressureValue, sourceText;
+end
