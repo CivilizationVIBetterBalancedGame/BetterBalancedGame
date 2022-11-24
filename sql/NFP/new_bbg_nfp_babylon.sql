4d813e9ee2710c0c6a16ca3ef69bfbc43a6d68b6
Vodairo <clement.beria@gmail.com>
2022-11-24 23:37:04 +0100

    Babylon rework

--- sql/NFP/new_bbg_nfp_babylon.sql
+++ sql/NFP/new_bbg_nfp_babylon.sql
@@ -1,52 +1,92 @@
 --==============================================================
---******			C I V I L I Z A T I O N S			  ******
+--******            C I V I L I Z A T I O N S             ******
 --==============================================================
 
 -- Remove free envoy on first district without free building.
 DELETE FROM TraitModifiers WHERE ModifierId IN ('TRAIT_FREE_ENVOY_WHEN_DISTRICT_MADE', 'TRAIT_FREE_ENVOY_WHEN_DISTRICT_MADE_SPECIFIC') AND TraitType='TRAIT_LEADER_HAMMURABI';
 
 -- Eureka to 65% (from 100%)
-INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
-    ('BBG_TRAIT_BABYLON_EUREKA', 'MODIFIER_PLAYER_ADJUST_TECHNOLOGY_BOOST');
-INSERT INTO ModifierArguments(ModifierId, Name, Value, Extra) VALUES
-    ('BBG_TRAIT_BABYLON_EUREKA', 'Amount', '25', '-1');
+-- INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
+--     ('BBG_TRAIT_BABYLON_EUREKA', 'MODIFIER_PLAYER_ADJUST_TECHNOLOGY_BOOST');
+-- INSERT INTO ModifierArguments(ModifierId, Name, Value, Extra) VALUES
+--     ('BBG_TRAIT_BABYLON_EUREKA', 'Amount', '25', '-1');
+-- DELETE FROM TraitModifiers WHERE TraitType='TRAIT_CIVILIZATION_BABYLON' AND ModifierID='TRAIT_EUREKA_INCREASE';
+-- INSERT INTO TraitModifiers(TraitType, ModifierID) VALUES
+--     ('TRAIT_CIVILIZATION_BABYLON', 'BBG_TRAIT_BABYLON_EUREKA');
+
+-- -- Science +1% per technology unlocked.
+-- INSERT INTO Modifiers(ModifierId, ModifierType, OwnerRequirementSetId)
+--     SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'MODIFIER_PLAYER_CITIES_ADJUST_CITY_YIELD_MODIFIER', 'BBG_UTILS_PLAYER_HAS_' || TechnologyType
+--     FROM Technologies;
+-- INSERT INTO ModifierArguments(ModifierId, Name, Value)
+--     SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'YieldType', 'YIELD_SCIENCE'
+--     FROM Technologies;
+-- INSERT INTO ModifierArguments(ModifierId, Name, Value)
+--     SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'Amount', '1'
+--     FROM Technologies;
+-- INSERT INTO TraitModifiers(TraitType, ModifierId)
+--     SELECT 'TRAIT_CIVILIZATION_BABYLON', 'BBG_BABYLON_SCIENCE_' || TechnologyType
+--     FROM Technologies;
+
+-- -- Babylon don't get Eureka boost from Free Inquiry
+-- INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
+--     ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'REQUIREMENTSET_TEST_ALL');
+-- --5.1 Fixed the free inq bug
+-- INSERT INTO Requirements(RequirementId, RequirementType) VALUES
+--     ('PLAYER_IS_NOT_BABYLON', 'REQUIREMENT_PLAYER_TYPE_MATCHES');
+-- INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
+--     ('PLAYER_IS_NOT_BABYLON', 'CivilizationType', 'CIVILIZATION_BABYLON_STK');
+-- UPDATE Requirements SET Inverse = '1' WHERE RequirementId = 'PLAYER_IS_NOT_BABYLON';
+-- INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
+--     ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'PLAYER_IS_NOT_BABYLON'),
+--     ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'REQUIRES_PLAYER_HAS_GOLDEN_AGE');
+-- UPDATE Modifiers SET OwnerRequirementSetId='PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON' WHERE ModifierId='COMMEMORATION_SCIENTIFIC_GA_BOOSTS';
+
+--24/11/22 Babylon get free eureka when building a library
+DELETE FROM TraitModifiers WHERE TraitType='TRAIT_CIVILIZATION_BABYLON' AND ModifierID='TRAIT_SCIENCE_DECREASE';
 DELETE FROM TraitModifiers WHERE TraitType='TRAIT_CIVILIZATION_BABYLON' AND ModifierID='TRAIT_EUREKA_INCREASE';
-INSERT INTO TraitModifiers(TraitType, ModifierID) VALUES
-    ('TRAIT_CIVILIZATION_BABYLON', 'BBG_TRAIT_BABYLON_EUREKA');
 
--- Science +1% per technology unlocked.
-INSERT INTO Modifiers(ModifierId, ModifierType, OwnerRequirementSetId)
-    SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'MODIFIER_PLAYER_CITIES_ADJUST_CITY_YIELD_MODIFIER', 'BBG_UTILS_PLAYER_HAS_' || TechnologyType
-    FROM Technologies;
-INSERT INTO ModifierArguments(ModifierId, Name, Value)
-    SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'YieldType', 'YIELD_SCIENCE'
-    FROM Technologies;
-INSERT INTO ModifierArguments(ModifierId, Name, Value)
-    SELECT 'BBG_BABYLON_SCIENCE_' || TechnologyType, 'Amount', '1'
-    FROM Technologies;
-INSERT INTO TraitModifiers(TraitType, ModifierId)
-    SELECT 'TRAIT_CIVILIZATION_BABYLON', 'BBG_BABYLON_SCIENCE_' || TechnologyType
-    FROM Technologies;
-
--- Babylon don't get Eureka boost from Free Inquiry
-INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
-    ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'REQUIREMENTSET_TEST_ALL');
---5.1 Fixed the free inq bug
-INSERT INTO Requirements(RequirementId, RequirementType) VALUES
-    ('PLAYER_IS_NOT_BABYLON', 'REQUIREMENT_PLAYER_TYPE_MATCHES');
-INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
-    ('PLAYER_IS_NOT_BABYLON', 'CivilizationType', 'CIVILIZATION_BABYLON_STK');
-UPDATE Requirements SET Inverse = '1' WHERE RequirementId = 'PLAYER_IS_NOT_BABYLON';
+INSERT INTO RequirementSets(RequirementSetId , RequirementSetType)
+    SELECT 'BBG_PLAYER_IS_HAMMURABI_IN_' || EraType, 'REQUIREMENTSET_TEST_ALL' FROM Eras;
 INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
-    ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'PLAYER_IS_NOT_BABYLON'),
-    ('PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON', 'REQUIRES_PLAYER_HAS_GOLDEN_AGE');
-UPDATE Modifiers SET OwnerRequirementSetId='PLAYER_HAS_GOLDEN_AGE_AND_NOT_BABYLON' WHERE ModifierId='COMMEMORATION_SCIENTIFIC_GA_BOOSTS';
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_ANCIENT', 'BBG_PLAYER_IS_IN_ERA_ANCIENT_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_ANCIENT', 'BBG_PLAYER_IS_NOT_IN_ERA_CLASSICAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_CLASSICAL', 'BBG_PLAYER_IS_IN_ERA_CLASSICAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_CLASSICAL', 'BBG_PLAYER_IS_NOT_IN_ERA_MEDIEVAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_MEDIEVAL', 'BBG_PLAYER_IS_IN_ERA_MEDIEVAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_MEDIEVAL', 'BBG_PLAYER_IS_NOT_IN_ERA_RENAISSANCE_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_RENAISSANCE', 'BBG_PLAYER_IS_IN_ERA_RENAISSANCE_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_RENAISSANCE', 'BBG_PLAYER_IS_NOT_IN_ERA_INDUSTRIAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_INDUSTRIAL', 'BBG_PLAYER_IS_IN_ERA_INDUSTRIAL_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_INDUSTRIAL', 'BBG_PLAYER_IS_NOT_IN_ERA_MODERN_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_MODERN', 'BBG_PLAYER_IS_IN_ERA_MODERN_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_MODERN', 'BBG_PLAYER_IS_NOT_IN_ERA_ATOMIC_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_ATOMIC', 'BBG_PLAYER_IS_IN_ERA_ATOMIC_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_ATOMIC', 'BBG_PLAYER_IS_NOT_IN_ERA_INFORMATION_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_INFORMATION', 'BBG_PLAYER_IS_IN_ERA_INFORMATION_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_INFORMATION', 'BBG_PLAYER_IS_NOT_IN_ERA_FUTURE_REQUIREMENT'),
+  ('BBG_PLAYER_IS_HAMMURABI_IN_ERA_FUTURE', 'BBG_PLAYER_IS_IN_ERA_FUTURE_REQUIREMENT');
+INSERT INTO RequirementSetRequirements(RequirementSetId , RequirementId)
+    SELECT  'BBG_PLAYER_IS_HAMMURABI_IN_' || EraType, 'BBG_PLAYER_IS_HAMMURABI_REQUIREMENT' FROM Eras;
 
---Palgum can be built if city has aqueduct or dam
---UPDATE Buildings SET RequiresAdjacentRiver = '0' WHERE BuildingType = 'BUILDING_PALGUM';
+INSERT INTO Requirements(RequirementId, RequirementType) VALUES
+    ('BBG_PLAYER_IS_HAMMURABI_REQUIREMENT', 'REQUIREMENT_PLAYER_TYPE_MATCHES');
+INSERT INTO RequirementArguments(RequirementId, Name, Value) VALUES
+    ('BBG_PLAYER_IS_HAMMURABI_REQUIREMENT', 'LeaderType', 'LEADER_HAMMURABI');
+
+INSERT INTO Modifiers(ModifierId, ModifierType, OwnerRequirementSetId) 
+    SELECT 'BBG_HAMMURABI_FREE_EUREKA_LIBRARY_' || EraType, 'MODIFIER_PLAYER_GRANT_RANDOM_TECHNOLOGY_BOOST_BY_ERA', 'BBG_PLAYER_IS_HAMMURABI_IN_' || EraType FROM Eras;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_HAMMURABI_FREE_EUREKA_LIBRARY_' || EraType, 'StartEraType', 'ERA_ANCIENT' FROM Eras;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_HAMMURABI_FREE_EUREKA_LIBRARY_' || EraType, 'EndEraType', EraType FROM Eras;
+INSERT INTO ModifierArguments(ModifierId, Name, Value)
+    SELECT 'BBG_HAMMURABI_FREE_EUREKA_LIBRARY_' || EraType, 'Amount', 1 FROM Eras;
+INSERT INTO BuildingModifiers(BuildingType, ModifierId)
+    SELECT 'BUILDING_LIBRARY', 'BBG_HAMMURABI_FREE_EUREKA_LIBRARY_' || EraType FROM Eras;
 
 --==============================================================
---******			         CITY-STATES                  ******
+--******                     CITY-STATES                  ******
 --==============================================================
 
 -- Babylon - Nalanda infinite technology re-suze fix.
@@ -62,15 +102,15 @@ DELETE FROM Modifiers WHERE ModifierId='MINOR_CIV_NALANDA_FREE_TECHNOLOGY';
 
 -- Attach the modifier to check for improvement to each player
 INSERT INTO Modifiers 
-	(ModifierId, ModifierType)
-	VALUES
-	('MINOR_CIV_NALANDA_MAHAVIHARA', 'MODIFIER_ALL_PLAYERS_ATTACH_MODIFIER');
+    (ModifierId, ModifierType)
+    VALUES
+    ('MINOR_CIV_NALANDA_MAHAVIHARA', 'MODIFIER_ALL_PLAYERS_ATTACH_MODIFIER');
 
 -- Modifier to actually check if the improvement is built, only done once
 INSERT INTO Modifiers 
-	(ModifierId, ModifierType, OwnerRequirementSetId, RunOnce, Permanent)
-	VALUES
-	('MINOR_CIV_NALANDA_MAHAVIHARA_TECH_ON_FIRST_BUILD', 'MODIFIER_PLAYER_GRANT_RANDOM_TECHNOLOGY', 'PLAYER_HAS_MAHAVIHARA', 1, 1);
+    (ModifierId, ModifierType, OwnerRequirementSetId, RunOnce, Permanent)
+    VALUES
+    ('MINOR_CIV_NALANDA_MAHAVIHARA_TECH_ON_FIRST_BUILD', 'MODIFIER_PLAYER_GRANT_RANDOM_TECHNOLOGY', 'PLAYER_HAS_MAHAVIHARA', 1, 1);
 
 INSERT INTO ModifierArguments
     (ModifierId, Name, Type, Value)
