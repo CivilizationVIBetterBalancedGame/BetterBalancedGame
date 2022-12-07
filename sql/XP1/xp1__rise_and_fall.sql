c3225da8ef33b48d60295462584fb534c978c296
Apeul <martinsla@live.fr>
2022-12-07 18:17:09 +0100

    Mapuche file changes + Chemamull get 1 prod at civil service

--- sql/XP1/xp1__rise_and_fall.sql
+++ sql/XP1/xp1__rise_and_fall.sql
@@ -110,23 +110,7 @@ INSERT OR IGNORE INTO ModifierArguments (ModifierId, Name, Value)
 --==================
 -- Mapuche
 --==================
--- Combat bonus against Golden Age Civs set to 5 instead of 10
-UPDATE ModifierArguments SET Value='5' WHERE ModifierId='TRAIT_TOQUI_COMBAT_BONUS_VS_GOLDEN_AGE_CIV';
--- Malon Raiders become Horseman replacement and territory bonus replaced with +1 movement
-UPDATE Units SET Combat=36 , Cost=90 , Maintenance=2 , BaseMoves=5 , PrereqTech='TECH_HORSEBACK_RIDING' , MandatoryObsoleteTech='TECH_SYNTHETIC_MATERIALS' WHERE UnitType='UNIT_MAPUCHE_MALON_RAIDER';
-INSERT OR IGNORE INTO UnitReplaces (CivUniqueUnitType , ReplacesUnitType)
-	VALUES ('UNIT_MAPUCHE_MALON_RAIDER' , 'UNIT_HORSEMAN');
-UPDATE UnitUpgrades SET UpgradeUnit='UNIT_CAVALRY' WHERE Unit='UNIT_MAPUCHE_MALON_RAIDER';
-DELETE FROM UnitAbilityModifiers WHERE ModifierId='MALON_RAIDER_TERRITORY_COMBAT_BONUS';
--- Chemamull Unique Improvement gets +1 Production (another at Civil Service Civic)
---INSERT OR IGNORE INTO Improvement_YieldChanges (ImprovementType , YieldType , YieldChange)
---	VALUES ('IMPROVEMENT_CHEMAMULL' , 'YIELD_PRODUCTION' , 1);
---INSERT OR IGNORE INTO Improvement_BonusYieldChanges (Id , ImprovementType , YieldType , BonusYieldChange , PrereqCivic)
---	VALUES ('203' , 'IMPROVEMENT_CHEMAMULL' , 'YIELD_PRODUCTION' , '1' , 'CIVIC_CIVIL_SERVICE');
 
--- 20/12/14 Chemamull's now allowed on volcanic soil
-INSERT OR IGNORE INTO Improvement_ValidFeatures (ImprovementType, FeatureType)
-	VALUES ('IMPROVEMENT_CHEMAMULL','FEATURE_VOLCANIC_SOIL');
 
 --==================
 -- Netherlands
