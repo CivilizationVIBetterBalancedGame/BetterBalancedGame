e18aefd811ba25dfc67166040daa46241ee8c974
Vodairo <clement.beria@gmail.com>
2022-12-11 18:42:02 +0100

    Delicate Arch (+2 adjacent faith)

--- sql/XP1/xp1__rise_and_fall.sql
+++ sql/XP1/xp1__rise_and_fall.sql
@@ -503,8 +503,11 @@ DELETE FROM StartBiasTerrains WHERE CivilizationType='CIVILIZATION_MAPUCHE' AND 
 
 
 --==============================================================
---******			W O N D E R S  (NATURAL)			  ******
+--******			W O N D E R S  (MAN-MADE)			  ******
 --==============================================================
+-- Statue Liberty from 4 to 3 diplo points
+UPDATE ModifierArguments SET Value='3' WHERE ModifierId='STATUELIBERTY_DIPLOVP' AND Name='Amount';
+
 -- scott research station can be built and works in tundra
 INSERT OR IGNORE INTO Building_ValidTerrains (BuildingType, TerrainType) VALUES
 	('BUILDING_AMUNDSEN_SCOTT_RESEARCH_STATION', 'TERRAIN_TUNDRA'),
@@ -513,40 +516,6 @@ UPDATE RequirementArguments SET Value='TERRAIN_TUNDRA' WHERE RequirementId='REQU
 -- St. Basil gives 1 relic
 INSERT OR IGNORE INTO BuildingModifiers (BuildingType, ModifierId) VALUES
 	('BUILDING_ST_BASILS_CATHEDRAL', 'WONDER_GRANT_RELIC_BBG');
---Matterhorn +2 down from +3
--- UPDATE ModifierArguments SET Value='0' WHERE ModifierId='ALPINE_TRAINING_COMBAT_HILLS' AND Name='Amount';
---23/08/22 no more combat bonus
-DELETE FROM UnitAbilityModifiers WHERE ModifierId='ALPINE_TRAINING_COMBAT_HILLS';
-DELETE FROM Modifiers WHERE ModifierId='ALPINE_TRAINING_COMBAT_HILLS';
-
-
-
---==============================================================
---******			W O N D E R S  (NATURAL)			  ******
---==============================================================
--- Eye of the Sahara gets 2 Food, 2 Production, and 2 Science
-UPDATE ModifierArguments SET Value='0' WHERE ModifierId='EYESAHARA_PRODUCTION_ATOMIC' AND Name='Amount';
-UPDATE ModifierArguments SET Value='0' WHERE ModifierId='EYESAHARA_SCIENCE_ATOMIC' AND Name='Amount';
-INSERT OR IGNORE INTO Feature_YieldChanges (FeatureType, YieldType, YieldChange)
-		VALUES ('FEATURE_EYE_OF_THE_SAHARA', 'YIELD_FOOD', 2);
-UPDATE Feature_YieldChanges SET YieldChange=2 WHERE FeatureType='FEATURE_EYE_OF_THE_SAHARA' AND YieldType='YIELD_PRODUCTION';
-INSERT OR IGNORE INTO Feature_YieldChanges (FeatureType, YieldType, YieldChange)
-	VALUES ('FEATURE_EYE_OF_THE_SAHARA', 'YIELD_SCIENCE', 2);
-UPDATE Feature_YieldChanges SET YieldChange=2 WHERE FeatureType='FEATURE_EYE_OF_THE_SAHARA' AND YieldType='YIELD_SCIENCE';
--- lake retba
-INSERT OR IGNORE INTO Feature_YieldChanges (FeatureType, YieldType, YieldChange)
-	VALUES ('FEATURE_LAKE_RETBA', 'YIELD_FOOD', 2);
-
-UPDATE Feature_YieldChanges SET YieldChange=2 WHERE FeatureType='FEATURE_UBSUNUR_HOLLOW' AND YieldType='YIELD_PRODUCTION';
-UPDATE Feature_YieldChanges SET YieldChange=2 WHERE FeatureType='FEATURE_UBSUNUR_HOLLOW' AND YieldType='YIELD_FOOD';
-
-
---==============================================================
---******			W O N D E R S  (MAN-MADE)			  ******
---==============================================================
--- Statue Liberty from 4 to 3 diplo points
-UPDATE ModifierArguments SET Value='3' WHERE ModifierId='STATUELIBERTY_DIPLOVP' AND Name='Amount';
-
 
 --==============================================================
 --******				    O T H E R					  ******
