df4910381b74d19acb88831ce309d171eaf65883
Apeul <martinsla@live.fr>
2023-04-17 00:39:10 +0200

    Delete costal bias from netherlands

--- sql/XP1/Netherlands.sql
+++ sql/XP1/Netherlands.sql
@@ -3,3 +3,5 @@
 --==================
 UPDATE Improvements SET ValidAdjacentTerrainAmount=2 WHERE ImprovementType='IMPROVEMENT_POLDER';
 UPDATE Units SET StrategicResource='RESOURCE_NITER' WHERE UnitType='UNIT_DE_ZEVEN_PROVINCIEN';
+
+DELETE FROM StartBiasTerrains WHERE CivilizationType='CIVILIZATION_NETHERLANDS' AND TerrainType='TERRAIN_COAST';
