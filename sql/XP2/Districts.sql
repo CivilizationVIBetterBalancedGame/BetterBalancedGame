e7ad09b1017d8396623292c4cc0b8bd50cf83b27
DESKTOP-OBSOQ1Q\marti <martinsla@live.fr>
2023-06-08 17:03:08 +0200

    canals to butress

--- sql/XP2/Districts.sql
+++ sql/XP2/Districts.sql
@@ -5,4 +5,5 @@ INSERT INTO CustomPlacement(ObjectType, Hash, PlacementFunction)
     SELECT Types.Type, Types.Hash, 'BBG_CANAL_CUSTOM_PLACEMENT'
     FROM Types WHERE Type = 'DISTRICT_CANAL';
 
+-- 07/06/23 : Canal to butress
 UPDATE Districts SET PrereqTech='TECH_BUTTRESS' WHERE DistrictType='DISTRICT_CANAL';
