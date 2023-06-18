b1da944140568f9dd206088942871a23623383b1
Vodairo <7153206+Vodairo@users.noreply.github.com>
2023-06-18 14:15:56 +0200

    Fix + Clean

--- sql/Base/base.sql
+++ sql/Base/base.sql
@@ -99,7 +99,7 @@ INSERT INTO Improvement_ValidTerrains(ImprovementType, TerrainType) VALUES
 -- 12/06/23 Beach Resort can be built on appealling tiles
 UPDATE Improvements SET MinimumAppeal=2 WHERE ImprovementType='IMPROVEMENT_BEACH_RESORT';
 --15/06/23 Beach resort get gold double the appeal and tourism based on that
-UPDATE Improvement_Tourism SET TourismSource='TOURISMSOURCE_GOLD';
+UPDATE Improvement_Tourism SET TourismSource='TOURISMSOURCE_GOLD' WHERE ImprovementType='IMPROVEMENT_BEACH_RESORT';
 UPDATE Improvements SET YieldFromAppealPercent=200 WHERE ImprovementType='IMPROVEMENT_BEACH_RESORT';
 
 
@@ -135,6 +135,7 @@ INSERT INTO TechnologyPrereqs (Technology, PrereqTech)
 
 
 --Removing any Extra Yields Mechanism
+/*
 CREATE TABLE Numbers(Number INT NOT NULL);
 INSERT INTO Numbers VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15);
 
@@ -162,7 +163,6 @@ INSERT INTO ModifierArguments(ModifierId, Name, Value)
 INSERT INTO ModifierArguments(ModifierId, Name, Value)
     SELECT 'MODIFIER_CITY_REMOVE_'||Numbers.Number||'_EXTRA_'||Yields.YieldType||'_BBG', 'Amount', -1
     FROM Numbers LEFT JOIN Yields;
-/*
 INSERT INTO Modifiers(ModifierId, ModifierType)
     SELECT 'MODIFIER_CIV_CITIES_REMOVE_EXTRA_'||Numbers.Number||'_'||Yields.YieldType||'_BBG', 'MODIFIER_PLAYER_CITIES_ATTACH_MODIFIER'
     FROM Numbers LEFT JOIN Yields;
@@ -176,8 +176,8 @@ INSERT INTO TraitModifiers(TraitType, ModifierId)
 INSERT INTO TraitModifiers(TraitType, ModifierId)
     SELECT 'MINOR_CIV_DEFAULT_TRAIT', 'MODIFIER_CIV_CITIES_REMOVE_EXTRA_'||Numbers.Number||'_'||Yields.YieldType||'_BBG'
     FROM Numbers LEFT JOIN Yields;
-*/
 DROP TABLE Numbers;
+*/
 
 --=======================================================================
 --******               Wonder+Terrain/Feature                      ******
