f5359da05d270b3894394b83a540494baf0421ed
Apeul <martinsla@live.fr>
2023-10-14 19:42:52 +0200

    Yongle doubled yield from projects + and yield per pop bonus to 0.7 science and 0.5 culture (from 0.5/0.3)

---
+++ sql/LP/Yongle.sql
@@ -0,0 +1,13 @@
+--==================
+-- Yongle
+--==================
+-- 5.2.5 Yongle nerf Pop require remain the same but reduce science/culture from 1 per pop to +0.5/+0.3 (double the inner science/culture per pop) and reduce gold from 2 to 1
+-- 14/10/23 from 0.5/0.3 to 0.7/0.5
+UPDATE ModifierArguments SET Value='0.7' WHERE ModifierId='YONGLE_SCIENCE_POPULATION' AND Name='Amount';
+UPDATE ModifierArguments SET Value='0.5' WHERE ModifierId='YONGLE_CULTURE_POPULATION' AND Name='Amount';
+UPDATE ModifierArguments SET Value='1' WHERE ModifierId='YONGLE_GOLD_POPULATION' AND Name='Amount';
+
+-- 14/10/23 doubled project yield
+UPDATE Project_YieldConversions SET PercentOfProductionRate=100 WHERE ProjectType='PROJECT_LIJIA_FOOD';
+UPDATE Project_YieldConversions SET PercentOfProductionRate=100 WHERE ProjectType='PROJECT_LIJIA_FAITH';
+UPDATE Project_YieldConversions SET PercentOfProductionRate=200 WHERE ProjectType='PROJECT_LIJIA_GOLD';
