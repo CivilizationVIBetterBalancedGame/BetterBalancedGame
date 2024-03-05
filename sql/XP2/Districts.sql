4574a98b469bfb3a03fb1c762d06d0edf333b5ad
khihdh <keven.bihan@free.fr>
2024-03-05 18:51:43 +0100

    dam and canal cost as much as aqueduc

--- sql/XP2/Districts.sql
+++ sql/XP2/Districts.sql
@@ -1,5 +1,5 @@
 -- Green District cost same as other district (from 81)
-UPDATE Districts SET Cost=54 WHERE DistrictType IN ('DISTRICT_CANAL', 'DISTRICT_DAM');
+UPDATE Districts SET Cost=36 WHERE DistrictType IN ('DISTRICT_CANAL', 'DISTRICT_DAM');
 -- Custom Placement for canal
 INSERT INTO CustomPlacement(ObjectType, Hash, PlacementFunction)
     SELECT Types.Type, Types.Hash, 'BBG_CANAL_CUSTOM_PLACEMENT'
