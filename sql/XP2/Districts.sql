82071ae54281c3b79c0c483804539b17e4621cbf
khihdh <keven.bihan@free.fr>
2024-03-05 20:38:15 +0100

    reduce neighborhood cost (54 -> 40)

--- sql/XP2/Districts.sql
+++ sql/XP2/Districts.sql
@@ -1,5 +1,7 @@
--- Green District cost same as other district (from 81)
+-- 05/03/2024 Green District cost same as other district (from 81)
 UPDATE Districts SET Cost=36 WHERE DistrictType IN ('DISTRICT_CANAL', 'DISTRICT_DAM');
+
+-- 05/03/2024 neighboo
 -- Custom Placement for canal
 INSERT INTO CustomPlacement(ObjectType, Hash, PlacementFunction)
     SELECT Types.Type, Types.Hash, 'BBG_CANAL_CUSTOM_PLACEMENT'
@@ -7,3 +9,6 @@ INSERT INTO CustomPlacement(ObjectType, Hash, PlacementFunction)
 
 -- 07/06/23 : Canal to butress
 UPDATE Districts SET PrereqTech='TECH_BUTTRESS' WHERE DistrictType='DISTRICT_CANAL';
+
+-- 05/03/2024 Reduce cost of Neighborhood
+UPDATE Districts SET Cost=40 WHERE DistrictType IN ('DISTRICT_NEIGHBORHOOD');
