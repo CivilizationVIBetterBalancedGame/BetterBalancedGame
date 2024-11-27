e5a3b0e87161e47465785f8e076c0ab88cf8f0f2
Apeul <martinsla@live.fr>
2024-11-27 18:28:56 +0100

    Huszar base cs reduced to 63

--- sql/XP2/new_bbg_hungary.sql
+++ sql/XP2/new_bbg_hungary.sql
@@ -2,8 +2,6 @@
 UPDATE ModifierArguments SET Value='1' WHERE ModifierId='LEVY_MILITARY_TWO_FREE_ENVOYS';
 -- no combat bonus for levied units
 -- DELETE FROM ModifierArguments WHERE ModifierId='RAVEN_LEVY_COMBAT' AND Name='Amount' AND Value='5';
--- Huszars only +2 combat strength from each alliance instead of 3
-UPDATE ModifierArguments SET Value='1' WHERE ModifierId='HUSZAR_ALLIES_COMBAT_BONUS';
 -- Black Army only +2 combat strength from adjacent levied units
 UPDATE ModifierArguments SET Value='2' WHERE ModifierId='BLACK_ARMY_ADJACENT_LEVY';
 -- Only 1 extra movement for levied units
@@ -22,16 +20,13 @@ INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
 INSERT INTO TraitModifiers(TraitType, ModifierId) VALUES
     ('TRAIT_CIVILIZATION_PEARL_DANUBE', 'BBG_ADDITIONAL_GREEN_CARD');*/
 
--- Huszar +1 by suzed city-states
+-- Huszar +2 by suzed city-states
 INSERT INTO Modifiers(ModifierId, ModifierType, SubjectRequirementSetId)
     SELECT 'BBG_MODIFIER_HUSZAR_SUZ_' || LeaderType, 'MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH', 'BBG_PLAYER_IS_SUZERAIN_OF_' || LeaderType || '_REQUIREMENTS'
     FROM Leaders
     WHERE InheritFrom IN
         ('LEADER_MINOR_CIV_CULTURAL', 'LEADER_MINOR_CIV_INDUSTRIAL', 'LEADER_MINOR_CIV_MILITARISTIC',
         'LEADER_MINOR_CIV_RELIGIOUS', 'LEADER_MINOR_CIV_SCIENTIFIC', 'LEADER_MINOR_CIV_TRADE');
-
---INSERT INTO Modifiers(ModifierId, ModifierType) VALUES
-    --('BBG_MODIFIER_HUSZAR_DISPLAY_MODIFIER','MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH');
 
 INSERT INTO ModifierArguments(ModifierId, Name, Value)
     SELECT 'BBG_MODIFIER_HUSZAR_SUZ_' || LeaderType, 'Amount', '2'
@@ -40,18 +35,12 @@ INSERT INTO ModifierArguments(ModifierId, Name, Value)
         ('LEADER_MINOR_CIV_CULTURAL', 'LEADER_MINOR_CIV_INDUSTRIAL', 'LEADER_MINOR_CIV_MILITARISTIC',
         'LEADER_MINOR_CIV_RELIGIOUS', 'LEADER_MINOR_CIV_SCIENTIFIC', 'LEADER_MINOR_CIV_TRADE');
 
---INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
-    --('BBG_MODIFIER_HUSZAR_DISPLAY_MODIFIER', 'Amount', '0');
-
 INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId)
     SELECT 'ABILITY_HUSZAR', 'BBG_MODIFIER_HUSZAR_SUZ_' || LeaderType
     FROM Leaders
     WHERE InheritFrom IN
         ('LEADER_MINOR_CIV_CULTURAL', 'LEADER_MINOR_CIV_INDUSTRIAL', 'LEADER_MINOR_CIV_MILITARISTIC',
         'LEADER_MINOR_CIV_RELIGIOUS', 'LEADER_MINOR_CIV_SCIENTIFIC', 'LEADER_MINOR_CIV_TRADE');
-
---INSERT INTO UnitAbilityModifiers(UnitAbilityType, ModifierId) VALUES
-    --('ABILITY_HUSZAR', 'BBG_MODIFIER_HUSZAR_DISPLAY_MODIFIER');
 
 DELETE FROM UnitAbilityModifiers WHERE ModifierId='HUSZAR_ALLIES_COMBAT_BONUS';
 
@@ -62,22 +51,4 @@ INSERT INTO ModifierStrings(ModifierId, Context, Text)
         ('LEADER_MINOR_CIV_CULTURAL', 'LEADER_MINOR_CIV_INDUSTRIAL', 'LEADER_MINOR_CIV_MILITARISTIC',
         'LEADER_MINOR_CIV_RELIGIOUS', 'LEADER_MINOR_CIV_SCIENTIFIC', 'LEADER_MINOR_CIV_TRADE');
 
-
--- Get 2 envoy when levy a city-state if there are a t2 building in gouv plaza
-
---INSERT INTO RequirementSets(RequirementSetId, RequirementSetType) VALUES
---    ('BBG_REQUIREMENT_HAS_T2_GOUV_PLAZA', 'REQUIREMENTSET_TEST_ANY');
---
---INSERT INTO RequirementSetRequirements(RequirementSetId, RequirementId) VALUES
---    ('BBG_REQUIREMENT_HAS_T2_GOUV_PLAZA', 'PLAYER_HAS_GOV_BUILDING_CITYSTATES_REQUIREMENT'),
---    ('BBG_REQUIREMENT_HAS_T2_GOUV_PLAZA', 'PLAYER_HAS_GOV_BUILDING_FAITH_REQUIREMENT'),
---    ('BBG_REQUIREMENT_HAS_T2_GOUV_PLAZA', 'PLAYER_HAS_GOV_BUILDING_SPIES_REQUIREMENT');
---
---INSERT INTO Modifiers(ModifierId, ModifierType, OwnerRequirementSetId) VALUES
---    ('BBG_LEVY_MILITARY_FREE_ENVOYS_T2_GOUV_PLAZA', 'MODIFIER_PLAYER_ADJUST_ENVOYS_FROM_LEVIED_CITY_STATES', 'BBG_REQUIREMENT_HAS_T2_GOUV_PLAZA');
---
---INSERT INTO ModifierArguments(ModifierId, Name, Value) VALUES
---    ('BBG_LEVY_MILITARY_FREE_ENVOYS_T2_GOUV_PLAZA', 'Amount', '1');
---
---INSERT INTO TraitModifiers(TraitType, ModifierId) VALUES
---    ('TRAIT_LEADER_RAVEN_KING', 'BBG_LEVY_MILITARY_FREE_ENVOYS_T2_GOUV_PLAZA');
+UPDATE Units SET Combat=63 WHERE UnitType='UNIT_HUNGARY_HUSZAR';
