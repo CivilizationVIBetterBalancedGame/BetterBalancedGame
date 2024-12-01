14fa97155ba8b7a070539f3b9e36393487392139
Apeul <martinsla@live.fr>
2024-12-01 17:31:22 +0100

    Sejong change

--- sql/LP/Sejong.sql
+++ sql/LP/Sejong.sql
@@ -8,7 +8,8 @@
 -- SEJONG_ATOMIC_SCIENCE_INTO_CULTURE
 -- SEJONG_INFORMATION_SCIENCE_INTO_CULTURE
 -- SEJONG_FUTURE_SCIENCE_INTO_CULTURE
-UPDATE ModifierArguments SET Value=1 WHERE ModifierId LIKE "SEJONG%SCIENCE_INTO_CULTURE" AND Name="Multiplier";
+-- 01/12/24 reverted to x2
+UPDATE ModifierArguments SET Value=2 WHERE ModifierId LIKE "SEJONG%SCIENCE_INTO_CULTURE" AND Name="Multiplier";
 
 -- Sejong exclusion from .5 adjacency
 INSERT INTO ExcludedAdjacencies(TraitType, YieldChangeId) VALUES
@@ -35,10 +36,11 @@ INSERT INTO TraitModifiers (TraitType, ModifierId) VALUES
 	('TRAIT_LEADER_SEJONG', 'BBG_SEJONG_TOURISM_PER_DISTRICTS');
 
 -- 04/07/23 Cities get +15% prod towards builders after feudalism and if the city has no gov
+-- 01/12/24 to +30%
 INSERT INTO Modifiers (ModifierId, ModifierType, OwnerRequirementSetId, SubjectRequirementSetId) VALUES
 	('BBG_SEJONG_BUILDER_PROD_NO_GOV', 'MODIFIER_PLAYER_UNITS_ADJUST_UNIT_PRODUCTION', 'BBG_UTILS_PLAYER_HAS_CIVIC_FEUDALISM_REQSET', 'CITY_HAS_NO_GOVERNOR_REQUIREMENTS');
 INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
 	('BBG_SEJONG_BUILDER_PROD_NO_GOV', 'UnitType', 'UNIT_BUILDER'),
-	('BBG_SEJONG_BUILDER_PROD_NO_GOV', 'Amount', 15);
+	('BBG_SEJONG_BUILDER_PROD_NO_GOV', 'Amount', 30);
 INSERT INTO TraitModifiers (TraitType, ModifierId) VALUES
 	('TRAIT_LEADER_SEJONG', 'BBG_SEJONG_BUILDER_PROD_NO_GOV');
