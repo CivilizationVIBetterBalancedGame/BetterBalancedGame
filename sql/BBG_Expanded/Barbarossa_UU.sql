9d2e674d2dc968e712bd8f5b00c73f4afa64c9fc
DESKTOP-OBSOQ1Q\marti <martinsla@live.fr>
2025-12-19 15:54:14 +0100

    Start of Barbarossa Ludwig changes

---
+++ sql/BBG_Expanded/Barbarossa_UU.sql
@@ -0,0 +1,23 @@
+
+-- Barbarossa tank replacement
+-- +3 base
+-- +3 when not adjacent to another panzer
+DELETE FROM UnitAbilityModifiers WHERE UnitAbilityType='ABILITY_PANZER';
+
+UPDATE Units SET Combat=88, Cost=480 WHERE UnitType='UNIT_GERMAN_PANZER';
+
+-- INSERT INTO Requirements (RequirementId, RequirementType, Inverse) VALUES
+-- 	('BBG_PANZER_IS_NOT_ADJACENT', 'REQUIREMENT_PLOT_ADJACENT_FRIENDLY_UNIT_TYPE_MATCHES', 1);
+-- INSERT INTO RequirementArguments (RequirementId, Name, Value) VALUES
+-- 	('BBG_PANZER_IS_NOT_ADJACENT', 'UnitType', 'UNIT_GERMAN_PANZER');
+-- INSERT INTO RequirementSets (RequirementSetId, RequirementSetType) VALUES
+-- 	('BBG_PANZER_IS_NOT_ADJACENT_REQSET', 'REQUIREMENTSET_TEST_ALL');
+-- INSERT INTO RequirementSetRequirements (RequirementSetId, RequirementId) VALUES
+-- 	('BBG_PANZER_IS_NOT_ADJACENT_REQSET', 'BBG_PANZER_IS_NOT_ADJACENT');
+
+-- INSERT INTO Modifiers (ModifierId, ModifierType, SubjectRequirementSetId) VALUES
+-- 	('BBG_PANZER_CS_NOT_ADJACENT_PANZER', 'MODIFIER_UNIT_ADJUST_COMBAT_STRENGTH', 'BBG_PANZER_IS_NOT_ADJACENT_REQSET');
+-- INSERT INTO ModifierArguments (ModifierId, Name, Value) VALUES
+-- 	('BBG_PANZER_CS_NOT_ADJACENT_PANZER', 'Amount', 3);
+-- INSERT INTO UnitAbilityModifiers (UnitAbilityType, ModifierId) VALUES
+-- 	('ABILITY_PANZER', 'BBG_PANZER_CS_NOT_ADJACENT_PANZER');
