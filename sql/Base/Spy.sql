6b6412f7ac201840713c03ae608537a13d05c57d
Pebbleton2 <sardo.lionel@gmail.com>
2026-03-16 21:13:22 +0100

    Spy mission duration

--- sql/Base/Spy.sql
+++ sql/Base/Spy.sql
@@ -2,8 +2,9 @@
 -- Listening post and counterspy last 50 turns (Online Speed)
 UPDATE UnitOperations SET Turns=100 WHERE OperationType='UNITOPERATION_SPY_COUNTERSPY' OR OperationType='UNITOPERATION_SPY_LISTENING_POST';
 
-
-UPDATE UnitOperations SET Turns=12 WHERE OperationType='UNITOPERATION_SPY_NEUTRALIZE_GOVERNOR' OR OperationType='UNITOPERATION_SPY_SABOTAGE_PRODUCTION';
+-- Neutralize governor and sabotage production to 6 turns (Online Speed)
+-- 16/03/26 Reduce neutralize governor and sabotage production to 5 turns (Online Speed)
+UPDATE UnitOperations SET Turns=10 WHERE OperationType='UNITOPERATION_SPY_NEUTRALIZE_GOVERNOR' OR OperationType='UNITOPERATION_SPY_SABOTAGE_PRODUCTION';
 
 -- 05/01/26 Deleted Breach Dam
 DELETE FROM UnitOperations WHERE OperationType='UNITOPERATION_SPY_BREACH_DAM';
